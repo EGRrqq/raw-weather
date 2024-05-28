47901b4e410563d550f45bb4fd9e0f90b7e8187c
EGRrqq <egrrqqdev@gmail.com>
2024-05-28 07:46:24 -0400

    feat: little top-left border for rain shader

--- src/shaders/triangle.fs
+++ src/shaders/triangle.fs
@@ -6,19 +6,29 @@ precision highp float;
 
 #define PI 3.14159265359
 vec4 mainColor = vec4(0.0f, 0.7f, 1.0f, 1.0f);
-vec4 shadeColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+vec4 shadeColorF = vec4(0.0f, 0.59f, 0.84f, 1.0f);
+vec4 shadeColorS = vec4(0.0f, 0.5f, 0.7f, 1.0f);
 
 uniform vec2 u_resolution;
 uniform float u_time;
+
 // we need to declare an output for the fragment shader
-out vec4 color;
+out vec4 FragColor;
 
 void main() {
-  // vec2 st = gl_FragCoord.xy / u_resolution.xy;
+  // spatial texture coords
+  // normalized coords of current pixel texture
+  vec2 st = gl_FragCoord.xy / u_resolution.xy;
 
-  float pct = abs(sin(u_time));
+  // percentage of shade gradient
+  float pct = abs(sin(u_time * 0.25f));
 
-  // Mix uses pct (a value from 0-1) to
-  // mix the two colors
-  color = mix(mainColor, shadeColor, pct);
+  // check if the pixel in a border range
+  if(st.x < 0.499f || st.y < 0.429f) {
+    // mix the two shade colors for border
+    FragColor = mix(shadeColorF, shadeColorS, pct);
+  } else {
+    // draw main shader color
+    FragColor = mainColor;
+  }
 }
