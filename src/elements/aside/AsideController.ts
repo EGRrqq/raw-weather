bf0d73c1774762e318af80b4e713f238591470dd
EGRrqq <egrrqqdev@gmail.com>
2024-12-03 09:57:46 -0500

    feat(aside): create input

--- src/elements/aside/AsideController.ts
+++ src/elements/aside/AsideController.ts
@@ -1,13 +1,27 @@
 import type { CanvasController } from "@/elements/canvas/CanvasController";
 import type { ICoords } from "@/webgl/interfaces/ICoords";
 
+type IAsideInputsMap = "weather-speed";
+
+interface IAsideInput {
+	el: HTMLInputElement;
+	eventCb: (e: Event) => void;
+}
+type IAsideInputs =
+	| Record<IAsideInputsMap, IAsideInput>
+	| Record<PropertyKey, never>;
+
 export class AsideController {
 	#observer: ResizeObserver;
+	#inputs: IAsideInputs = {};
 	initCoords: ICoords = { x: 0, y: 0 };
 
 	constructor(canvasController: CanvasController) {
 		// set init coords
 		this.#updateInitCoords();
+
+		// create inputs
+		this.#createInput("weather-speed", () => console.log("we movin"));
 
 		// resize canvas on aside resize
 		this.#observer = new ResizeObserver(() => this.#init(canvasController));
@@ -23,6 +37,20 @@ export class AsideController {
 
 		return aside;
 	}
+
+	#createInput = (id: IAsideInputsMap, cb: IAsideInput["eventCb"]) => {
+		const wrapper = document.createElement("section");
+
+		const inputEl = document.createElement("input");
+		inputEl.id = id;
+		inputEl.name = id;
+		inputEl.type = "range";
+		this.#inputs[id] = { el: inputEl, eventCb: cb };
+		inputEl.addEventListener("input", cb);
+
+		wrapper.appendChild(inputEl);
+		this.#aside.appendChild(wrapper);
+	};
 
 	#updateInitCoords = () => {
 		this.initCoords.x = -this.#aside.clientWidth / 2;
